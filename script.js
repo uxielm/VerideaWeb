992e651b5c81df6c8aeb5f231c9cc9e810780163
darak0z <darakowo@gmail.com>
2026-09-01 17:34:10 -0400

    mover a script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 six = "seven"
+
+document.addEventListener('DOMContentLoaded', () => {
+    // función para el boton de "Copiar IP"
+    const btnCopyIp = document.getElementById('btn-copy-ip');
+    if(btnCopyIp) {
+        btnCopyIp.addEventListener('click', (event) => {
+            const ipText = document.getElementById('server-ip-text').innerText;
+            
+            // copiar
+            navigator.clipboard.writeText(ipText).then(() => {
+                const btn = event.target;
+                const originalText = btn.innerText;
+                
+                // feedback visual
+                btn.innerText = 'Copiado';
+                
+                // volver al texto original despues de 1.5s
+                setTimeout(() => {
+                    btn.innerText = originalText;
+                }, 1500);
+            }).catch(err => {
+                console.error('Error al copiar el texto: ', err);
+            });
+        });
+    }
+});
